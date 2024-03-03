40d90091f667c61216b6cb072c99b7a3a5ed5ed0
jhessica <jhessfsantos@gmail.com>
2024-03-03 18:05:09 -0300

    adicionando projeto

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,8 @@ import { Pagination } from "./components/pagination"
 import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import { useSearchParams } from "react-router-dom"
 import { useState } from "react"
+import * as Dialog from "@radix-ui/react-dialog"
+import { CreateTagForm } from "./components/create-tag-form"
 
 export interface TagResponse {
   first: number
@@ -35,8 +37,8 @@ export interface Tag {
 
 export function App() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const urlFilter = searchParams.get("filter") ?? ''
-  
+  const urlFilter = searchParams.get("filter") ?? ""
+
   const [filter, setFilter] = useState(urlFilter)
 
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1
@@ -78,10 +80,30 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Creat new
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Creat new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+
+                <CreateTagForm />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
 
         <div className="flex items-center justify-between">
